4f2c5b0a8445e621b34be45490d48408fbe7dfaf
names <337503908@qq.com>
2019-09-11 12:25:54 +0800

    测试

--- src/main/java/ocm/qs/demo/textgit.java
+++ src/main/java/ocm/qs/demo/textgit.java
@@ -8,10 +8,12 @@ public class textgit {
         System.out.println(11111111);
         System.out.println(11111111);
         System.out.println(11111111);
-
+        System.out.println("下载成功   继续测试");
     }
 
     public void add(){
         System.out.println("222222222222222");
+
+        System.out.println("下载成功   继续测试");
     }
 }
