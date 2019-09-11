f7e877eb104ddc9f504bf58055c1583791f1993b
names <337503908@qq.com>
2019-09-11 12:03:44 +0800

    本地工程添加到仓库测试

---
+++ src/main/java/ocm/qs/demo/textgit.java
@@ -0,0 +1,13 @@
+package ocm.qs.demo;
+
+public class textgit {
+    public static void main(String[] args) {
+        System.out.println(11111111);
+        System.out.println(11111111);
+        System.out.println(11111111);
+        System.out.println(11111111);
+        System.out.println(11111111);
+        System.out.println(11111111);
+
+    }
+}
